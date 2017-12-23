2c437cb1ef4ddbbd0ea0b462fff1c19263280168
Brian Dyko <briandyko@gmail.com>
2017-12-22 21:40:16 -0500

    initial test commit

---
+++ FirstConsoleAppOnNewComputer/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FirstConsoleAppOnNewComputer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 0; i < 100; i++)
+            {
+                Console.WriteLine("Hello Leslie, I can write programs on this now.");
+                Console.WriteLine("See......");
+                Console.WriteLine(i);
+                Console.WriteLine("Git Config Test");
+            }
+        }
+    }
+}
